61c31956fbf82bdb0460542dc9b844e01881b6c1
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-15 21:56:06 +0700

    Update body parser

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configLogger } from '@relationc/logger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(await configLogger(app))
   app.setGlobalPrefix('/api/v1');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
